407434e9cf9cd7651bdf01c6183ec3de8b51edaf
nikta <nikta.akbarpour99gmail.com>
2021-01-15 10:57:18 +0330

    created real-time date and time

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../assets/logo.png';
 import {makeStyles} from "@material-ui/core/styles";
+import {DateAndTime} from "./DateAndTime";
 
 const useStyles = makeStyles((theme) => ({
         name: {
@@ -12,21 +13,36 @@ const useStyles = makeStyles((theme) => ({
             // marginLeft: "200px"
         },
         image: {
-            marginTop: "20px",
-            marginLeft: "20px"
+            marginTop: theme.spacing(3),
+            width: theme.spacing(30),
+            marginLeft: theme.spacing(2),
+            height:theme.spacing(10)
         },
         container: {
-            display: "flex"
-        }
+            display: "flex",
+            justifyContent: "space-between"
+        },
+    dateAndTime: {
+            marginTop: theme.spacing(5),
+        marginRight: theme.spacing(2)
+    }
     })
 );
 
 export default function Header() {
     const classes = useStyles();
+    const {date, time} = DateAndTime();
     return (
         <div className={classes.container}>
             <img className={classes.image} height='100px' width='100px' src={logo} alt="Logo"/>
-            <p className={classes.name}>Fake news!</p>
+            <p className={classes.name}>Veritical news!</p>
+            <div>
+                <h3 className={classes.dateAndTime}>
+                    {date}
+                    <br />
+                    {time}
+                </h3>
+            </div>
         </div>
     )
 }
