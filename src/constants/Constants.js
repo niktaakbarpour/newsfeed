29843d6b58e1293d5d843ceacf5cb3b2aacbc48a
aria <09130088640m@gmail.com>
2021-01-17 19:31:06 +0330

    prepare for real connection to server

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,5 +1,5 @@
 //URLS
-export const BASE_URL = 'http://25.105.127.25:8080'
+export const BASE_URL = ''
 export const FACEBOOK_URL = 'https://facebook.com'
 export const INSTAGRAM_URL = 'https://instagram.com'
 export const TWITTER_URL = 'https://twitter.com'
